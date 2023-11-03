ddd21be3f79299e7089d2b98a57cb29370105c1e
Yegor Beloussov <gorien@mail.ru>
2023-11-03 18:27:51 +0600

    Вылечил ЛА

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -858,7 +858,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
@@ -2476,7 +2476,7 @@
 /**
  * enable/disable debug function
  */
-#define ENABLE_DEBUG_FUNC     1     /* 0:disable, 1:enable */
+#define ENABLE_DEBUG_FUNC     0     /* 0:disable, 1:enable */
 
 /**
  * firmware type
